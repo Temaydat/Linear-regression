c7a85f91aa6e8a9f8d32413be6ce73e1dd2ba58d
Mary Temmy <temaydat@gmail.com>
2023-12-14 01:51:24 +1000

    predict_api

--- app.py
+++ app.py
@@ -14,15 +14,28 @@ def home():
 
 @app.route('/predict_api', methods = ['POST'])
 def predict_api():
-    data = request.get_json('data')
-    print(data)
-    data_value = np.array(list(data.values())).reshape(-1,1)
-    print(data_value)
-    new_data = scaler.transform(data_value)
-    output = regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    try:
+        data = request.get_json()['data']
+        print(data)
+        
+        # Extract values from the dictionary and convert to array
+        data_values = [data[key] for key in sorted(data.keys())]  # Assuming keys are 'x2', 'x5', 'x6', 'x8'
+        data_array = np.array(data_values).reshape(1, -1)
 
+        print(data_array)
+        
+        # Assuming 'scaler' is your scaler object
+        new_data = scaler.transform(data_array)
+        
+        # Assuming 'regmodel' is your regression model
+        output = regmodel.predict(new_data)
+        
+        print(output[0])
+        
+        return jsonify(output[0])
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
